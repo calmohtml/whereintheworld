baf755084bf920e4261e426b1b8d57fb5fe7bc0b
calmohtml <contactosegovia@protonmail.com>
2022-04-12 14:37:49 -0300

    Inplementing search bar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import { ActivityIndicator } from "react-native";
-import React from "react";
+import { TextInput, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { HeadingOne, HeadingTwo, HeadingThree } from "../styles/GlobalStyles";
 import {
@@ -17,6 +17,7 @@ export const HomeScreen = ({ navigation }) => {
   const BASE_URL = "https://restcountries.com/v3.1";
   const { data, loading, error } = useFetch(`${BASE_URL}/all`);
   const numbro = require("numbro");
+  const [text, setText] = useState("");
 
   if (error) {
     console.log(error);
@@ -28,48 +29,63 @@ export const HomeScreen = ({ navigation }) => {
           <ActivityIndicator size="large" color="#fafafa" />
         ) : (
           <List>
-            {data.map(({ cca3, name, flags, population, region, capital }) => (
-              <CountryContainer
-                key={cca3}
-                onPress={() => {
-                  navigation.navigate("Country", {
-                    countryName: name.common,
-                  });
-                }}
-              >
-                <CountryFlagContainer>
-                  <CountryFlag source={{ uri: `${flags.png}` }} />
-                </CountryFlagContainer>
-                <CountryName>
-                  <HeadingOne>{name.common}</HeadingOne>
-                </CountryName>
-                <CountryInfo>
-                  <HeadingTwo>Population: </HeadingTwo>
-                  <HeadingThree>
-                    {numbro(population).format({
-                      thousandSeparated: true,
-                    })}
-                  </HeadingThree>
-                </CountryInfo>
-                <CountryInfo>
-                  <HeadingTwo>Region: </HeadingTwo>
-                  <HeadingThree>{region}</HeadingThree>
-                </CountryInfo>
-                {capital !== undefined ? (
+            <TextInput
+              defaultValue={text}
+              placeholder="Search"
+              onChangeText={(newText) => setText(newText)}
+            />
+            {data
+              .filter(({ name }) => {
+                if (text == "") {
+                  return name.common;
+                } else if (
+                  name.common.toLowerCase().includes(text.toLowerCase())
+                ) {
+                  return name.common;
+                }
+              })
+              .map(({ cca3, name, flags, population, region, capital }) => (
+                <CountryContainer
+                  key={cca3}
+                  onPress={() => {
+                    navigation.navigate("Country", {
+                      countryName: name.common,
+                    });
+                  }}
+                >
+                  <CountryFlagContainer>
+                    <CountryFlag source={{ uri: `${flags.png}` }} />
+                  </CountryFlagContainer>
+                  <CountryName>
+                    <HeadingOne>{name.common}</HeadingOne>
+                  </CountryName>
                   <CountryInfo>
-                    <HeadingTwo>Capital: </HeadingTwo>
-                    {capital.map((capital) => (
-                      <HeadingThree key={capital}>{capital} </HeadingThree>
-                    ))}
+                    <HeadingTwo>Population: </HeadingTwo>
+                    <HeadingThree>
+                      {numbro(population).format({
+                        thousandSeparated: true,
+                      })}
+                    </HeadingThree>
                   </CountryInfo>
-                ) : (
                   <CountryInfo>
-                    <HeadingTwo>Capital: </HeadingTwo>
-                    <HeadingThree>None</HeadingThree>
+                    <HeadingTwo>Region: </HeadingTwo>
+                    <HeadingThree>{region}</HeadingThree>
                   </CountryInfo>
-                )}
-              </CountryContainer>
-            ))}
+                  {capital !== undefined ? (
+                    <CountryInfo>
+                      <HeadingTwo>Capital: </HeadingTwo>
+                      {capital.map((capital) => (
+                        <HeadingThree key={capital}>{capital} </HeadingThree>
+                      ))}
+                    </CountryInfo>
+                  ) : (
+                    <CountryInfo>
+                      <HeadingTwo>Capital: </HeadingTwo>
+                      <HeadingThree>None</HeadingThree>
+                    </CountryInfo>
+                  )}
+                </CountryContainer>
+              ))}
           </List>
         )}
       </Container>
